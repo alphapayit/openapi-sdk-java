1dd0eb4b94ab17dadf00e1e0ffef5f9e31b38d9b
Markus <markus.zhang@alphapay.com>
2025-04-16 11:13:18 -0700

    chore: add payment request id support

--- src/main/java/com/alphapay/api/request/beans/SearchOrderRequest.java
+++ src/main/java/com/alphapay/api/request/beans/SearchOrderRequest.java
@@ -7,12 +7,22 @@ public class SearchOrderRequest extends AlphaPayRequest<SearchOrderResponse> {
 
     private String paymentId;
 
+    private String paymentRequestId;
+
     public String getPaymentId() {
         return paymentId;
     }
 
     public void setPaymentId(String paymentId) {
         this.paymentId = paymentId;
+    }
+
+    public String getPaymentRequestId() {
+        return paymentRequestId;
+    }
+
+    public void setPaymentRequestId(String paymentRequestId) {
+        this.paymentRequestId = paymentRequestId;
     }
 
     @Override
